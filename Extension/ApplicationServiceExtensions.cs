96c68e5e911d5b43180b471449601f40262ac099
Vilens Senat <n01064501@unf.edu>
2025-05-31 12:33:41 -0400

    cleanup program file using an extension class library to register services

---
+++ Extension/ApplicationServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Database.Data;
+using Database.Interface;
+using DataService.Interface;
+using DataService.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Models.Dtos.User;
+using Auth.Jwt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+
+            // adding the database context and connection string
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+                config.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("Database")));
+
+            //dependency injections
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddTransient<IUserService, UserService>();
+
+
+            //database context for dependency injection
+            services.AddTransient<IAppDbContext, AppDbContext>();
+
+
+            //instanciating mapping profiles
+            services.AddAutoMapper(typeof(UserMappingProfile));
+
+
+            return services;
+        }
+    }
+}
---
+++ Extension/CorsServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class CorsServiceExtensions
+    {
+        public static IServiceCollection CorsServices(this IServiceCollection services)
+        {
+            //adding cors policy
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins("http://localhost:4200");
+                });
+            });
+
+            return services;
+        }
+    }
+}
