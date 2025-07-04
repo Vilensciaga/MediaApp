f8eb58d0e7e25a7d1eeb40eff5de9abdcd2ae781
Vilens Senat <n01064501@unf.edu>
2025-07-04 16:50:24 -0400

    added picture

--- client/src/app/components/members/member-detail/member-detail.component.ts
+++ client/src/app/components/members/member-detail/member-detail.component.ts
@@ -3,18 +3,46 @@ import { Member } from '../../../models/member';
 import { MembersService } from '../../../services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import {CommonModule} from '@angular/common';
+import { GalleryModule, GalleryItem, ImageItem, GalleryConfig, ThumbnailsPosition, VideoItem, YoutubeItem, IframeItem, } from 'ng-gallery';
+import { map, Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-member-detail',
-  imports: [CommonModule],
+  imports: [CommonModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
 export class MemberDetailComponent implements OnInit{
   member!:Member;
+  images: GalleryItem[] = [];
+   galleryConfig$!: Observable<GalleryConfig>;
 
-  constructor(private memberService:MembersService, private route:ActivatedRoute){}
+
+
+  constructor(private memberService:MembersService, private route:ActivatedRoute, private breakpointObserver: BreakpointObserver)
+  {
+    this.galleryConfig$ = breakpointObserver.observe([
+      Breakpoints.HandsetPortrait
+    ]).pipe(
+      map(res => {
+        if (res.matches) {
+          return {
+            thumbPosition: ThumbnailsPosition.Top,
+            thumbWidth: 80,
+            thumbHeight: 80
+          };
+        }
+        return {
+          thumbPosition: ThumbnailsPosition.Left,
+          thumbWidth: 120,
+          thumbHeight: 90
+        };
+      })
+    );
+
+  }
   ngOnInit(): void {
-    this.loadMember();
+    this.loadMember(); 
   }
 
   loadMember()
@@ -26,11 +54,25 @@ export class MemberDetailComponent implements OnInit{
             {
               next:member=>{
                 this.member = member;
+                this.loadImages();
+
               }
             }
           );
     }
-    
+  }
+
+  loadImages()
+  {
+    for(const photo of this.member.photos)
+    {
+      this.images.push(
+        new ImageItem({
+          src: photo.url,
+          thumb: photo.url
+        })
+      );
+    }
   }
 
 }
