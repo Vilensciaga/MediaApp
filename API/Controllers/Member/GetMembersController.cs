9d8eab412244615cc2f87ca544ccc42127c46ba1
Vilens Senat <n01064501@unf.edu>
2025-08-08 15:04:02 -0400

    Added Getmembers using clean architecture

---
+++ API/Controllers/Member/GetMembersController.cs
@@ -0,0 +1,35 @@
+﻿using Extensions.AppExtensions;
+using F23.Kernel;
+using Helpers.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models.Dtos.User;
+using UseCases.GetMembers;
+
+namespace API.Controllers.Member
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class GetMembersController(IQueryHandler<GetMembersQuery, GetMembersQueryResult> queryHandler) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetMembersAsync([FromRoute] UserParams userParams)
+        {
+            string username = User.GetUsername();
+
+            var query = new GetMembersQuery
+            {
+                Username = username,
+                UserParams = userParams
+            };
+
+            var result = await queryHandler.Handle(query);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ UseCases/GetMembers/GetMembersQueryResult.cs
@@ -0,0 +1,15 @@
+﻿using Helpers.Helpers.PageList;
+using Models.Dtos.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases.GetMembers
+{
+    public class GetMembersQueryResult
+    {
+        public IPagedList<MemberDto>? Members { get; init; }
+    }
+}
